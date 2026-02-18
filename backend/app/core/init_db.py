fef4e3b2903b7b9c9d3a6431697d3d0219b81a55
Awais <awanawais400@gmail.com>
2026-02-18 22:50:46 +0500

    Integrated alembic migrations

--- backend/app/core/init_db.py
+++ backend/app/core/init_db.py
@@ -3,7 +3,7 @@ from app.core.database import engine, Base
 from app.models.user import User
 
 def init():
-    Base.metadata.create_all(bind=engine)
+    # Base.metadata.create_all(bind=engine)    "Database initialization now handled by Alembic."
 
 if __name__ == "__main__":
     init()
